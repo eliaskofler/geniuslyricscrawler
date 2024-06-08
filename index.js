011627f1110423c5113931d39fa415ba17a3b772
E. Kofler <eliaskofler@icloud.com>
2024-06-08 09:23:07 -0700

    added blacklist

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const mysql = require('mysql2/promise');
-const fs = require('fs');
+const fs = require('fs').promises;
 const http = require('http');
 const path = require('path');
 const delay = (time) => new Promise(resolve => setTimeout(resolve, time));
@@ -42,6 +42,8 @@ async function lyricsCrawling(p, dbconn) {
     try {
         console.log("crawling..");
         const url = "https://genius.com/Udo-jurgens-griechischer-wein-lyrics";
+        const blacklist = await fs.readFile('blacklist.txt', 'utf-8');
+        const blacklistArray = blacklist.split('\n').map(line => line.trim()).filter(line => line.length > 0);
 
         await p.goto(url);
         await delay(1000);
@@ -66,6 +68,36 @@ async function lyricsCrawling(p, dbconn) {
         }
 
 
+        // Wait for the elements with the specified attribute to be loaded
+        await p.waitForSelector('div[data-lyrics-container="true"]');
+
+        // Extract the content of all matching elements and join them into a single string
+        const content = await p.evaluate(() => {
+            const elements = document.querySelectorAll('div[data-lyrics-container="true"]');
+            return Array.from(elements).map(element => {
+            // Replace <br> with newline characters
+            element.querySelectorAll('br').forEach(br => br.replaceWith('\n'));
+            return element.textContent;
+            }).join('\n');
+        });
+
+        console.log('Content:', content);
+
+
+        await p.waitForSelector('a');
+
+        // Extract all href attributes from anchor tags
+        const hrefs = await p.evaluate(() => {
+            const anchorElements = document.querySelectorAll('a');
+            return Array.from(anchorElements).map(anchor => anchor.href);
+        });
+
+        // Filter out the hrefs that are in the blacklist
+        const filteredHrefs = hrefs
+            .filter(href => href.includes('genius.com'))
+            .filter(href => !blacklistArray.includes(href));
+
+        console.log('Filtered Hrefs:', filteredHrefs);
 
     } catch(error) {
         console.log(error);

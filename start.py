735e8033f0a26642c98b507e6e27b47be5fb3835
Elias Kofler <eliaskofler@icloud.com>
2024-06-17 00:57:21 -0700

    Update start.py

--- start.py
+++ start.py
@@ -5,10 +5,9 @@ def run_nodejs_file(file_path):
     try:
         process = subprocess.Popen(['node', file_path])
         print("Node.js process started.")
-        time.sleep(600)  # Wait for 5 minutes
+        time.sleep(600)
         print("Shutting down Node.js process...")
-        process.terminate()  # Terminate the Node.js process
-        process.wait()  # Wait for the process to terminate
+        process.kill()
     except Exception as e:
         print(f"Error occurred: {e}")
         print("Restarting the Node.js file...")
@@ -18,7 +17,6 @@ if __name__ == "__main__":
     nodejs_file_path = "index.js"
     
     while True:
-        # Run the Node.js file for 5 minutes
         run_nodejs_file(nodejs_file_path)
         print("Waiting for 10 seconds before restarting...")
-        time.sleep(10)  # Wait for 10 seconds before restarting
+        time.sleep(10)

33c1fffab775b7764e688eff6bebd5490d32c2e4
Elias Kofler <eliaskofler@icloud.com>
2024-06-17 01:08:21 -0700

    update stat site

--- datasite/progress/getdata.php
+++ datasite/progress/getdata.php
@@ -21,34 +21,34 @@ if ($conn->connect_error) {
 }
 
 // Initialize variables for counting albums_fetched
-$albums_fetched_0 = 0;
-$albums_fetched_1 = 0;
+$album_songs_fetched_0 = 0;
+$album_songs_fetched_1 = 0;
 
 // Counting albums_fetched for artists
-$sql = "SELECT albums_fetched, COUNT(*) AS count FROM artists GROUP BY albums_fetched";
+$sql = "SELECT fetched, COUNT(*) AS count FROM albums GROUP BY fetched";
 $result = $conn->query($sql);
 
 if ($result) {
     while ($row = $result->fetch_assoc()) {
-        if ($row['albums_fetched'] == 0) {
-            $albums_fetched_0 = $row['count'];
-        } elseif ($row['albums_fetched'] == 1) {
-            $albums_fetched_1 = $row['count'];
+        if ($row['fetched'] == 0) {
+            $album_songs_fetched_0 = $row['count'];
+        } elseif ($row['fetched'] == 1) {
+            $album_songs_fetched_1 = $row['count'];
         }
     }
 } else {
-    die(json_encode(array("error" => "Error fetching albums_fetched counts: " . $conn->error)));
+    die(json_encode(array("error" => "Error fetching fetched counts: " . $conn->error)));
 }
 
 // Initialize an array to store the counts
 $dataCounts = array();
 
 // Calculate done_percentage
-$done_percentage = ($albums_fetched_1 / ($albums_fetched_0 + $albums_fetched_1)) * 100;
+$done_percentage = ($album_songs_fetched_1 / ($album_songs_fetched_0 + $album_songs_fetched_1)) * 100;
 $done_percentage = round($done_percentage) . "%";
 
 // Add total count and additional counts to the data counts array
-$dataCounts = array_merge(array("albums_fetched_0" => $albums_fetched_0, "albums_fetched_1" => $albums_fetched_1, "done_percentage" => $done_percentage), $dataCounts);
+$dataCounts = array_merge(array("album_songs_fetched_0" => $album_songs_fetched_0, "album_songs_fetched_1" => $album_songs_fetched_1, "done_percentage" => $done_percentage), $dataCounts);
 
 // Close the connection
 $conn->close();
